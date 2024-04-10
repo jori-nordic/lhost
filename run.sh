9af89c8de85b47f6cd70d52614a3386f3be57f1c
Rico, Jonathan <jonathan.rico@nordicsemi.no>
2024-04-10 15:32:01 +0200

    Verify advertising works
    ..it works!
    
    The zephyr observer sample is able to display the packets.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-# set -e
+
+# We don't want to execute if we have a build error
+set -eu
 
 uart_h2c=/tmp/repl/myid/uart.h2c
 uart_c2h=/tmp/repl/myid/uart.c2h
@@ -9,7 +11,13 @@ pushd $(west topdir)/lhost/apps/hci_sim
 west build -b nrf52_bsim
 popd
 
-hci_uart="$(west topdir)/lhost/apps/hci_sim/build/zephyr/zephyr.exe"
+# Build scanner image
+pushd $(west topdir)/lhost/apps/observer
+west build -b nrf52_bsim
+popd
+
+# It's ok if the FIFO already exists
+set +eu
 
 mkdir -p $(dirname ${uart_h2c})
 mkfifo ${uart_h2c}
@@ -19,12 +27,15 @@ mkfifo ${uart_c2h}
 ~/sdk/bsim/components/common/stop_bsim.sh
 
 # This talks to the REPL
+hci_uart="$(west topdir)/lhost/apps/hci_sim/build/zephyr/zephyr.exe"
 $hci_uart \
     -s=myid -d=1 -RealEncryption=0 -rs=70 \
     -fifo_0_rx=${uart_h2c} \
     -fifo_0_tx=${uart_c2h} &
 
-# TODO: start peer device
+# Start peer device
+observer="$(west topdir)/lhost/apps/observer/build/zephyr/zephyr.exe"
+$observer -s=myid -d=2 -RealEncryption=0 -rs=70 &
 
 # Force sim to (kinda) real-time
 pushd "${BSIM_COMPONENTS_PATH}/device_handbrake"
@@ -32,4 +43,4 @@ pushd "${BSIM_COMPONENTS_PATH}/device_handbrake"
 
 # Start the PHY
 pushd "${BSIM_OUT_PATH}/bin"
-./bs_2G4_phy_v1 -s=myid -D=2
+./bs_2G4_phy_v1 -s=myid -D=3
